26002f4dcd83c9c589988ff453e8ac251d459b4e
pmdusso <pmdusso@8f7a1285-3d0f-a319-eb9c-673de53f0f44>
2010-12-03 17:22:12 +0000

    Pedro Dusso - teste do meu checkin

--- containerloading/src/Model/BTabu.java
+++ containerloading/src/Model/BTabu.java
@@ -5,10 +5,34 @@
 
 package Model;
 
+import java.util.Queue;
+
 /**
  *
  * @author Pedro
  */
-public class BTabu extends java.util.LinkedList{
+public class BTabu{
 
+    private int listLimit;
+    private Queue<Box> listaTabu;
+
+    public BTabu(int _numberOfBoxTypes)
+    {
+        listLimit = _numberOfBoxTypes;
+    }
+    public int listTabuLimit()
+    {
+        return listLimit;
+    }
+    public int listTabuNumberOfBoxes()
+    {
+        return listaTabu.size();
+    }
+    public boolean addBox(Box _box)
+    {
+        //se a lista estiver cheia, remove a ultima solucao valida
+        if(listaTabu.size() == listLimit)
+            listaTabu.remove();
+        return listaTabu.add(_box);
+    }
 }
