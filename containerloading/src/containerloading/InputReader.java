e3651be4765cd476beec0d0bcb95d91fb5128a8b
otaviokz <otaviokz@8f7a1285-3d0f-a319-eb9c-673de53f0f44>
2010-12-05 13:24:56 +0000

    Criado readInput2(), adaptado para ler os arquivos brx.txt, que são os corretos.

--- containerloading/src/containerloading/InputReader.java
+++ containerloading/src/containerloading/InputReader.java
@@ -81,10 +81,67 @@ public class InputReader {
         }
     }
 
+
+     public void readInputBR(Integer instanceNumber) {
+        File input = new File(filePath);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(input));
+
+            String instancesNumber = reader.readLine().trim();
+            Integer numberOfInstances = Integer.parseInt(instancesNumber);
+
+            for (int i = 0; i < numberOfInstances; i++) {
+                String instanceData = reader.readLine().trim();
+                String[] idSeed = instanceData.split(" ");
+                Integer id = Integer.parseInt(idSeed[0]);
+
+                seed = Long.parseLong(idSeed[1]);
+                String containerData = reader.readLine().trim();
+                String[] containerDataArray = containerData.split(" ");
+
+                Integer conatinerWidth = Integer.parseInt(containerDataArray[0]);
+                Integer conatinerlength = Integer.parseInt(containerDataArray[1]);
+                Integer conatinerHeight = Integer.parseInt(containerDataArray[2]);
+
+                String boxTypesNumberStr = reader.readLine().trim();
+                boxTypes = Integer.parseInt(boxTypesNumberStr);
+
+
+                for (int j = 0; j < boxTypes; j++) {
+                    String boxData = reader.readLine().trim();
+                    String[] boxDataArray = boxData.split(" ");
+
+                    Integer numberOfBoxes = Integer.parseInt(boxDataArray[1]);
+
+                    Boolean widthVertical = !Boolean.parseBoolean(boxDataArray[2]);
+                    Integer boxWidth = Integer.parseInt(boxDataArray[3]);
+
+                    Boolean lengthVertical = !Boolean.parseBoolean(boxDataArray[4]);
+                    Integer boxLength = Integer.parseInt(boxDataArray[5]);
+
+                    Boolean heigthVertical = !Boolean.parseBoolean(boxDataArray[6]);
+                    Integer boxHeigth = Integer.parseInt(boxDataArray[7]);
+
+                    if (id == instanceNumber) {
+                        addBox(boxWidth, widthVertical, boxLength, lengthVertical, boxHeigth, heigthVertical, numberOfBoxes, j);
+
+                    }
+                }
+
+                if (id == instanceNumber) {
+                    this.container = new Container(new Vector3d(conatinerWidth, conatinerlength, conatinerHeight));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Houve um erro na hora de ler o arquivo. \n" + e.getMessage());
+        }
+    }
+
     private void addBox(Integer boxWidth, Boolean widthVertical,
             Integer boxLength, Boolean lengthVertical,
             Integer boxHeigth, Boolean heigthVertical,
-            Integer numberOfBoxes, Integer boxType) {
+            Integer numberOfBoxes, Integer boxType)
+    {
 
         Box box = new Box(new Vector3d(boxWidth, boxLength, boxHeigth),
                 widthVertical, lengthVertical, heigthVertical, boxType);
@@ -94,6 +151,7 @@ public class InputReader {
         for (int i = 0; i < numberOfBoxes; i++) {
             boxes.add(box);
         }
+        System.out.println("Added " + numberOfBoxes + " bofes of type: " + boxType + "(" + boxWidth + "," + boxLength +"," + boxHeigth + ")");
     }
 
     public List<Box> getBoxes() {
