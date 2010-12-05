ee07e6f24367cfbe77953b122c35ac510ed0ff70
otaviokz <otaviokz@8f7a1285-3d0f-a319-eb9c-673de53f0f44>
2010-12-05 21:01:16 +0000

    inputReadBR retirado

--- containerloading/src/containerloading/InputReader.java
+++ containerloading/src/containerloading/InputReader.java
@@ -80,61 +80,7 @@ public class InputReader {
             System.out.println("<readInput> Houve um erro na hora de ler o arquivo. \n" + e.toString());
         }
     }
-
-    public void readInputBR(Integer instanceNumber) {
-        File input = new File(filePath);
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(input));
-
-            String instancesNumber = reader.readLine().trim();
-            Integer numberOfInstances = Integer.parseInt(instancesNumber);
-
-            for (int i = 0; i < numberOfInstances; i++) {
-                String instanceData = reader.readLine().trim();
-                String[] idSeed = instanceData.split(" ");
-                Integer id = Integer.parseInt(idSeed[0]);
-
-                seed = Long.parseLong(idSeed[1]);
-                String containerData = reader.readLine().trim();
-                String[] containerDataArray = containerData.split(" ");
-
-                Integer conatinerWidth = Integer.parseInt(containerDataArray[0]);
-                Integer conatinerlength = Integer.parseInt(containerDataArray[1]);
-                Integer conatinerHeight = Integer.parseInt(containerDataArray[2]);
-
-                String boxTypesNumberStr = reader.readLine().trim();
-                boxTypes = Integer.parseInt(boxTypesNumberStr);
-
-
-                for (int j = 0; j < boxTypes; j++) {
-                    String boxData = reader.readLine().trim();
-                    String[] boxDataArray = boxData.split(" ");
-
-                    Integer numberOfBoxes = Integer.parseInt(boxDataArray[1]);
-
-                    Boolean widthVertical = !Boolean.parseBoolean(boxDataArray[2]);
-                    Integer boxWidth = Integer.parseInt(boxDataArray[3]);
-
-                    Boolean lengthVertical = !Boolean.parseBoolean(boxDataArray[4]);
-                    Integer boxLength = Integer.parseInt(boxDataArray[5]);
-
-                    Boolean heigthVertical = !Boolean.parseBoolean(boxDataArray[6]);
-                    Integer boxHeigth = Integer.parseInt(boxDataArray[7]);
-
-                    if (id == instanceNumber) {
-                        addBox(boxWidth, widthVertical, boxLength, lengthVertical, boxHeigth, heigthVertical, numberOfBoxes, j);
-
-                    }
-                }
-
-                if (id == instanceNumber) {
-                    this.container = new Container(new Vector3d(conatinerWidth, conatinerlength, conatinerHeight));
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("<readInputBR> Houve um erro na hora de ler o arquivo. \n" + e.toString());
-        }
-    }
+   
 
     private void addBox(Integer boxWidth, Boolean widthVertical,
             Integer boxLength, Boolean lengthVertical,
